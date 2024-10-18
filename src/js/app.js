1de8d42716819e9eb0ad6e3c356e86f069a7dbf2
Mykhailo Pilat <m.pilat@ukma.edu.ua>
2024-10-18 06:56:40 +0300

    Lab1 completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-//const testModules = require('./test-module');
-//require('../css/app.css');
+// const testModules = require('./test-module');
+// require('../css/app.css');
 
 /** ******** Your code here! *********** */
 const buttons = document.querySelectorAll('.add-teacher-button');
@@ -16,56 +16,56 @@ const prevButton = document.querySelector('.left-arrow-button');
 const nextButton = document.querySelector('.right-arrow-button');
 
 const togglePopup = (popup) => {
-    popup.classList.toggle('popup--active');
-    const isActive = popup.classList.contains('popup--active');
-    popup.setAttribute('aria-hidden', !isActive);
-}
+  popup.classList.toggle('popup--active');
+  const isActive = popup.classList.contains('popup--active');
+  popup.setAttribute('aria-hidden', !isActive);
+};
 
-buttons.forEach(button =>{
-    button.addEventListener('click', () => {
-        togglePopup(popupAddTeacher);
-    });
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    togglePopup(popupAddTeacher);
+  });
 });
 
-closeButtons.forEach(closeBtn => {
-    closeBtn.addEventListener('click', () => {
-        if (popupAddTeacher.classList.contains('popup--active')) {
-            togglePopup(popupAddTeacher);
-        } else if (popupTeacherInfo.classList.contains('popup--active')) {
-            togglePopup(popupTeacherInfo);
-        }
-    });
+closeButtons.forEach((closeBtn) => {
+  closeBtn.addEventListener('click', () => {
+    if (popupAddTeacher.classList.contains('popup--active')) {
+      togglePopup(popupAddTeacher);
+    } else if (popupTeacherInfo.classList.contains('popup--active')) {
+      togglePopup(popupTeacherInfo);
+    }
+  });
 });
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        togglePopup(popupTeacherInfo);
-        const profileImageFrom = card.querySelector('.zoom-image');
-        if (profileImageFrom) {
-            const profileImageSrc = profileImageFrom.getAttribute('src');
-            profileImage.style.backgroundImage = `url(${profileImageSrc})`;
-        } else {
-            profileImage.style.backgroundImage = 'url(./images/placeholder.png)';
-        }
-        const cardNameElements = card.querySelectorAll('h4');
-        const firstName = cardNameElements[0].textContent;
-        const lastName = cardNameElements[1].textContent;
-        profileName.textContent = `${firstName} ${lastName}`;
-    });
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    togglePopup(popupTeacherInfo);
+    const profileImageFrom = card.querySelector('.zoom-image');
+    if (profileImageFrom) {
+      const profileImageSrc = profileImageFrom.getAttribute('src');
+      profileImage.style.backgroundImage = `url(${profileImageSrc})`;
+    } else {
+      profileImage.style.backgroundImage = 'url(./images/placeholder.png)';
+    }
+    const cardNameElements = card.querySelectorAll('h4');
+    const firstName = cardNameElements[0].textContent;
+    const lastName = cardNameElements[1].textContent;
+    profileName.textContent = `${firstName} ${lastName}`;
+  });
 });
 
 nextButton.addEventListener('click', () => {
-    carousel.scrollBy({
-        left: 300, 
-        behavior: 'smooth' 
-    });
+  carousel.scrollBy({
+    left: 300,
+    behavior: 'smooth',
+  });
 });
 
 prevButton.addEventListener('click', () => {
-    carousel.scrollBy({
-        left: -300, 
-        behavior: 'smooth' 
-    });
+  carousel.scrollBy({
+    left: -300,
+    behavior: 'smooth',
+  });
 });
 
-//console.log(testModules.hello);
+// console.log(testModules.hello);
